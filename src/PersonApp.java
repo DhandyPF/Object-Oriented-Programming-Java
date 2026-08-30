3d4ce43830188285bc55e442ae2e357ebb0f330e
Dhandy Putra Fahruddin <dhandy.pf@gmail.com>
2026-08-30 21:48:06 +0700

    Belajar Method di Java

--- src/PersonApp.java
+++ src/PersonApp.java
@@ -1,6 +1,7 @@
 public class PersonApp {
     static void main() {
         var person1 = new Person();
+
         person1.name = "Dhandy";
         person1.address = "Cirebon";
 //        person1.country = "India";
@@ -8,13 +9,15 @@ public class PersonApp {
         System.out.println(person1.address);
         System.out.println(person1.country);
 
+        person1.sayHello("Dimas");
+
         Person person2 = new Person();
 
         Person person3;
         person3 = new Person();
 
-        System.out.println(person1);
-        System.out.println(person2);
-        System.out.println(person3);
+//        System.out.println(person1);
+//        System.out.println(person2);
+//        System.out.println(person3);
     }
 }
