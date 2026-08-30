39a9fc6edc8505a8389c86a93ef33db71e35556d
Dhandy Putra Fahruddin <dhandy.pf@gmail.com>
2026-08-30 21:55:48 +0700

    Belajar Constructor di Java

--- src/PersonApp.java
+++ src/PersonApp.java
@@ -1,20 +1,17 @@
 public class PersonApp {
     static void main() {
-        var person1 = new Person();
-
-        person1.name = "Dhandy";
-        person1.address = "Cirebon";
+        var person1 = new Person("Dhandy", "Cirebon");
 //        person1.country = "India";
         System.out.println(person1.name);
         System.out.println(person1.address);
         System.out.println(person1.country);
 
-        person1.sayHello("Dimas");
+        person1.sayHello("A");
 
-        Person person2 = new Person();
+        Person person2 = new Person("Budi", "Kuningan");
 
-        Person person3;
-        person3 = new Person();
+//        Person person3;
+//        person3 = new Person();
 
 //        System.out.println(person1);
 //        System.out.println(person2);
