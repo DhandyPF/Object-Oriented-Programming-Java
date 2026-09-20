1e910de1bfa8a18aa5979f65513fc644c69111a6
Dhandy Putra Fahruddin <dhandy.pf@gmail.com>
2026-09-21 00:03:10 +0700

    Belajar tentang Package di Java

---
+++ src/programmer/it/data/Product.java
@@ -0,0 +1,11 @@
+package programmer.it.data;
+
+class Product {
+    String name;
+    int price;
+
+    Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
